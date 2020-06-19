f2b33f356d269f3f0cbd72a7f2b959da6ecd3399
nicobo1026 <1026register@gmail.com>
2020-06-19 20:18:03 +0900

    add plugin "now loading"
    now loadingをアニメーションするプラグインを導入しました。
    now loadingアニメーション用の画像データを追加しました。

--- js/plugins.js
+++ js/plugins.js
@@ -3,6 +3,6 @@
 var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"基本的なパラメーターを設定するプラグインです。","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
-{"name":"MadeWithMv","status":false,"description":"メイン画面へ進む前に、\"Made with MV\"のスプラッシュ画面もしくはカスタマイズされたスプラッシュ画面を表示します。","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"60","Fade In Time":"120","Wait Time":"160"}},
-{"name":"Mano_StandingImage","status":false,"description":"表情連動型立ち絵プラグインです。\r\nピクチャーに張り付けた顔を文章の表示コマンドから制御します。","parameters":{"testData":"","list":"","list2":"","actorSetting":"[]","statePriority":"[]","defaultOpacity":"255","usingEditMode":"","editModeSymbol":"StandingImageEdit"}}
+{"name":"--------------------------","status":true,"description":"---------------------------------------------------------------","parameters":{}},
+{"name":"LoadingExtend","status":true,"description":"ロード中画像拡張プラグイン","parameters":{"イメージ列数":"4","イメージ行数":"1","表示タイプ":"2","アニメーション間隔":"10","表示位置X座標":"","表示位置Y座標":"","待機フレーム数":"10","点滅なし":"ON"}}
 ];
---
+++ js/plugins/--------------------------.js
@@ -0,0 +1,9 @@
+﻿/*:
+ * NOTE: Images are stored in the img/system folder.
+ *
+ * @plugindesc ---------------------------------------------------------------
+ * @author Nagi
+ *
+ * @help プラグイン一覧での境界線を表示します。
+ *
+ */
